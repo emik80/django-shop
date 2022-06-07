1fe8c6431cdf9b3ec33aa184e667c644526c7c75
emik80 <yudkin@gmail.com>
2022-06-07 11:18:28 +0300

    First commit

--- mysite/mysite/post_settings.py
+++ mysite/mysite/post_settings.py
@@ -1,8 +1,8 @@
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your@email'  # email for contact form message sending
-EMAIL_HOST_PASSWORD = 'yourpasword'  # password for contact form message sending
+EMAIL_HOST_USER = 'youremail'  # email for contact form message sending
+EMAIL_HOST_PASSWORD = 'yourpassword'  # password for contact form message sending
 EMAIL_USE_TLS = True
 
-RECIPIENT_LIST = ['your@email', ]  # recipient list for contact form message sending
+RECIPIENT_LIST = ['youremail', ]  # recipient list for contact form message sending
